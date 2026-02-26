336be6e7d5cf3430aaf4d27f200a85be13f83f2d
Fedor Kapranov <fedor.kapranov.06@gmail.com>
2026-02-26 06:27:44 +0300

    js-ts-task-01-solution

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,17 @@
 interface Todo {
-
+    id: number
+    title: string
+    description: string
+    status: TodoStatus
+    readonly createdAt: Date
 }
 
 enum TodoStatus {
-
+    COMPLETED = 0,
+    PENDING = 1,
+    IN_PROGRESS = 2
 }
+
+export type NewTodo = Omit<Todo, "id" | "createdAt">
 
 export { Todo, TodoStatus };
