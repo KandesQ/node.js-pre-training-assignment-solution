1cbd0c40dcadc0d4f6ae7ba46f5d0e985fde1d16
Fedor Kapranov <fedor.kapranov.06@gmail.com>
2026-02-27 13:23:59 +0300

    js-ts-task-04-solution

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,36 @@
 import { Todo } from './types';
 
-export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+export function addTodo(state: Todo[], newTodo: Todo): Todo[] {
+  return [...state, newTodo]
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+  const updatedState: Todo[] = []
+
+  for (const todo of state) {
+    updatedState.push(
+        todo.id === id
+          ? {...todo, ...update}
+          : todo
+    )
+  }
+
+  if (!updatedState.some(todo => todo.id === id)) {
+    throw new Error(`Id=[${id}] was not found in todos`)
+  }
+
+  return updatedState
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  const todoExists = state.some(todo => todo.id === id)
+  if (!todoExists) {
+      throw new Error(`Id=[${id}] was not found in todos`)
+  }
+
+  return state.filter(todo => todo.id !== id)
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  return state.find(todo => todo.id === id)
 }
