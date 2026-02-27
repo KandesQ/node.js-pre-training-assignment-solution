fb47249da0a2aa8738a3cf362a83f1d06c7f5950
Fedor Kapranov <fedor.kapranov.06@gmail.com>
2026-02-27 11:22:09 +0300

    js-ts-task-02-solution

--- JS-TS/solutions/array-helpers.ts
+++ JS-TS/solutions/array-helpers.ts
@@ -3,21 +3,79 @@
 // All helpers are declared but not implemented.
 
 export function mapArray<T, R>(source: readonly T[], mapper: (item: T, index: number) => R): R[] {
-  throw new Error('mapArray: not implemented');
+  sourceIsNotNullOrUndefined(source)
+
+  const res: R[] = []
+  for (let i = 0, len = source.length; i < len; i++) {
+    res.push(mapper(source[i], i))
+  }
+
+  return res
 }
 
 export function filterArray<T>(source: readonly T[], predicate: (item: T, index: number) => boolean): T[] {
-  throw new Error('filterArray: not implemented');
+  sourceIsNotNullOrUndefined(source)
+
+  const res: T[] = []
+  for (let i = 0, len = source.length; i < len; i++) {
+    if (predicate(source[i], i)) {
+
+      res.push(source[i])
+    }
+  }
+
+  return res
 }
 
 export function reduceArray<T, R>(source: readonly T[], reducer: (acc: R, item: T, index: number) => R, initial: R): R {
-  throw new Error('reduceArray: not implemented');
+  sourceIsNotNullOrUndefined(source)
+
+  let local_acc = initial
+
+  for (let i = 0; i < source.length; i++) {
+    local_acc = reducer(local_acc, source[i], i)
+  }
+
+  return local_acc
 }
 
 export function partition<T>(source: readonly T[], predicate: (item: T) => boolean): [T[], T[]] {
-  throw new Error('partition: not implemented');
+  sourceIsNotNullOrUndefined(source)
+
+  const passed: T[] = []
+  const failed: T[] = []
+
+  for (let i = 0; i < source.length; i++) {
+    if (predicate(source[i])) {
+      passed.push(source[i])
+    } else {
+      failed.push(source[i])
+    }
+  }
+
+  return [passed, failed]
 }
 
 export function groupBy<T, K extends PropertyKey>(source: readonly T[], keySelector: (item: T) => K): Record<K, T[]> {
-  throw new Error('groupBy: not implemented');
+  sourceIsNotNullOrUndefined(source)
+
+  const resultRecord: Record<K, T[]> = {} as Record<K, T[]> 
+  for (const entity of source) {
+    const k: K = keySelector(entity)
+
+    if (!resultRecord[k]) {
+      resultRecord[k] = []
+    }
+
+    resultRecord[k].push(entity)
+  }
+
+  return resultRecord
+}
+
+
+function sourceIsNotNullOrUndefined<T>(source: readonly T[] | null | undefined) {
+  if (source == null) {
+    throw new TypeError("Source is null or undefined")
+  }
 }
