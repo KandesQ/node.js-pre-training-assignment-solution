36926ee17e8f6216724ccff12f80bec571ec9f38
Fedor Kapranov <fedor.kapranov.06@gmail.com>
2026-02-27 22:32:11 +0300

    js-ts-task-06-solution and js-ts-task-07-solution

--- JS-TS/solutions/repository.ts
+++ JS-TS/solutions/repository.ts
@@ -3,22 +3,67 @@ export class InMemoryRepository<T extends { id: number }> {
   private items: T[] = [];
 
   add(entity: T): T {
-    throw new Error('add: not implemented');
+    const entityClone = structuredClone(entity)
+    this.items = [...this.items, entityClone]
+
+    return entityClone
   }
 
   update(id: number, patch: Partial<T>): T {
-    throw new Error('update: not implemented');
+    let updatedItem: T
+
+    this.items = this.items.map(
+        item => {
+          if (item.id === id) {
+            updatedItem = {...item, ...patch}
+            return updatedItem
+          }
+          return item
+        }
+    )
+
+    if (!updatedItem) {
+      throw new TodoNotFoundError(`Todo with id=[${id}] was not found`)
+    }
+
+    return updatedItem
   }
 
   remove(id: number): void {
-    throw new Error('remove: not implemented');
+    const newItems = this.items.filter(item => item.id !== id)
+
+    if (newItems.length === this.items.length) {
+      throw new TodoNotFoundError(`Todo with id=[${id}] was not found`)
+    }
+
+    this.items = newItems
   }
 
   findById(id: number): T | undefined {
-    throw new Error('findById: not implemented');
+    return structuredClone(
+        this.items.find(item => item.id === id)
+    )
   }
 
   findAll(): T[] {
-    throw new Error('findAll: not implemented');
+    return this.items.map(
+        item => structuredClone(item)
+    )
+  }
+
+  getMaxId(): number {
+    return this.items.reduce(
+        (acc, item) => Math.max(acc, item.id),
+        0
+    )
+  }
+}
+
+class TodoNotFoundError extends Error {
+  constructor(msg: string) {
+    super(msg)
+    this.name = "TodoNotFoundError"
+
+    Object.setPrototypeOf(this, TodoNotFoundError.prototype)
   }
 }
--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -5,18 +5,45 @@ export class TodoApi {
   private repo = new InMemoryRepository<Todo>();
 
   async getAll(): Promise<Todo[]> {
-    throw new Error('getAll: not implemented');
+    await randomDelay()
+    return this.repo.findAll()
   }
 
   async add(newTodo: NewTodo): Promise<Todo> {
-    throw new Error('add: not implemented');
+    const createdTodo: Todo = {
+      id: this.repo.getMaxId(),
+      title: newTodo.title,
+      description: newTodo.description,
+      status: newTodo.status,
+      createdAt: new Date()
+    }
+
+    await randomDelay()
+    return this.repo.add(createdTodo)
   }
 
   async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    throw new Error('update: not implemented');
+    await randomDelay()
+    return this.repo.update(id, update)
   }
 
   async remove(id: number): Promise<void> {
-    throw new Error('remove: not implemented');
+    await randomDelay()
+    this.repo.remove(id)
   }
+}
+
+
+/**
+ * Creates a random delay from 300 ms to 600 ms
+ *
+ * @param {number} min - minimal delay in milliseconds
+ * @param {number} max - maximum delay in milliseconds
+ */
+async function randomDelay(min: number = 300, max: number = 600) {
+  const delay = Math.floor(Math.random() * (max - min + 1) + min)
+
+  return new Promise(
+      resolve => setTimeout(resolve, delay)
+  )
 }
